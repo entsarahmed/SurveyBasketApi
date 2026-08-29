da49a596dc00a035712b139e3e602fe705aa2edf
entsarahmed <Doctor@DESKTOP-HMG0CU3>
2026-08-29 15:20:01 +0300

    Route Templates

---
+++ SurveyBasket.Api/Controllers/PollsController.cs
@@ -0,0 +1,23 @@
+﻿
+namespace SurveyBasket.Api.Controllers
+{
+    [Route("api/[controller]")]//  /api/polls
+    [ApiController]
+    public class PollsController : ControllerBase
+    {
+        private readonly List<Poll> _polls = [];
+
+        [HttpGet]
+        //[HttpGet("getAll2")]
+        [Route("")]
+        public IActionResult GetAll()
+        {
+            return Ok(_polls);
+        }
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok(_polls);
+        }
+    }
+}
