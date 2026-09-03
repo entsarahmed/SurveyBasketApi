d26f99924fad775c6fb370cbf3f0733f9e3dc24a
entsarahmed <Doctor@DESKTOP-HMG0CU3>
2026-09-03 15:00:50 +0300

    Add Validations

--- SurveyBasket.Api/Contracts/Validations/PollRequestValidator.cs
+++ SurveyBasket.Api/Contracts/Validations/PollRequestValidator.cs
@@ -14,6 +14,22 @@
             RuleFor(x => x.Summary)
                 .NotEmpty()
                 .Length(3, 1500);
+
+            RuleFor(x => x.StartsAt)
+                .NotEmpty()
+                .GreaterThanOrEqualTo(DateOnly.FromDateTime(DateTime.Today));
+
+            RuleFor(x => x.EndsAt)
+                .NotEmpty();
+
+            RuleFor(x => x)
+                .Must(HasValidDates)
+                .WithName(nameof(PollRequest.EndsAt))
+                .WithMessage("{PropertyName} must be greater than or equals start date");
+        }
+        private bool HasValidDates(PollRequest pollRequest)
+        {
+            return pollRequest.EndsAt >= pollRequest.StartsAt;
         }
     }
 }
