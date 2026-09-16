02a018fb5db294ca2c51f4d236b25c288eef123c
entsarahmed <Doctor@DESKTOP-HMG0CU3>
2026-09-16 21:00:55 +0300

    Add RefreshTokens Table

--- SurveyBasket.Api/Persistence/EntitiesConfigurations/UserConfiguration.cs
+++ SurveyBasket.Api/Persistence/EntitiesConfigurations/UserConfiguration.cs
@@ -9,6 +9,11 @@
 
             builder.Property(x => x.LastName)
                 .HasMaxLength(100);
+
+            builder.OwnsMany(x => x.RefreshTokens)
+                .ToTable("RefreshTokens")
+                .WithOwner()
+                .HasForeignKey("UserId");
         }
     }
 }
