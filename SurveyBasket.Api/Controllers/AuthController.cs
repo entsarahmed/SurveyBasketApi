5aff6d5a0f07a72c99f0c0b5824fed12bb25ea47
entsarahmed <Doctor@DESKTOP-HMG0CU3>
2026-09-18 14:05:54 +0300

    Revoke Refresh Token

--- SurveyBasket.Api/Controllers/AuthController.cs
+++ SurveyBasket.Api/Controllers/AuthController.cs
@@ -13,7 +13,7 @@ namespace SurveyBasket.Api.Controllers
        
 
         [HttpPost("")]
-        public async Task<IActionResult> LoginAsync(LoginRequest request, CancellationToken cancellationToken) 
+        public async Task<IActionResult> LoginAsync([FromBody]LoginRequest request, CancellationToken cancellationToken) 
         {
 
             var authResult = await _authService.GetTokenAsync(request.Email, request.Password, cancellationToken);
@@ -24,13 +24,21 @@ namespace SurveyBasket.Api.Controllers
 
 
         [HttpPost("refresh")]
-        public async Task<IActionResult> RefreshAsync(RefreshTokenRequest request, CancellationToken cancellationToken)
+        public async Task<IActionResult> RefreshAsync([FromBody]RefreshTokenRequest request, CancellationToken cancellationToken)
         {
             var authResult = await _authService.GetRefreshTokenAsync(request.Token, request.RefreshToken,cancellationToken);
 
             return authResult is null ? BadRequest("Invalid token") : Ok(authResult);
+   
+        }
 
-               
+        [HttpPost("revoke-refresh-token")]
+        public async Task<IActionResult> RevokeRefreshTokenAsync([FromBody] RefreshTokenRequest request, CancellationToken cancellationToken)
+        {
+            var isRevoked = await _authService.RevokeRefreshTokenAsync(request.Token, request.RefreshToken, cancellationToken);
+
+            return isRevoked ? Ok() : BadRequest("Operation failed");
+
         }
         
     }
--- SurveyBasket.Api/Services/IAuthService.cs
+++ SurveyBasket.Api/Services/IAuthService.cs
@@ -7,5 +7,9 @@ namespace SurveyBasket.Api.Services
         Task<AuthResponse?> GetTokenAsync(string email, string password, CancellationToken cancellationToken = default);
 
         Task<AuthResponse?> GetRefreshTokenAsync(string token, string refreshToken, CancellationToken cancellationToken = default);
+
+        Task<bool> RevokeRefreshTokenAsync(string token, string refreshToken, CancellationToken cancellationToken = default);
+
+    
     }
 }
